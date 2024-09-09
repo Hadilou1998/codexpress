e44bcdffb7841666c63147d371d63605c7d01eee
Hadilou1998 <hadilissilame@gmail.com>
2024-09-09 15:29:54 +0200

    Updated Note.php

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NoteRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Note
 {
     #[ORM\Id]
@@ -54,6 +55,19 @@ class Note
     public function __construct()
     {
         $this->notifications = new ArrayCollection();
+    }
+
+    #ORM\PrePersist
+    public function setCreatedAtValue(): void
+    {
+        $this->created_at = new \DateTimeImmutable();
+        $this->setUpdatedAtValue();
+    }
+
+    #ORM\PreUpdate
+    public function setUpdatedAtValue(): void
+    {
+        $this->updated_at = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
