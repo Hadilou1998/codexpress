40110e56fd9f50a8a620acc3142ff6b764709919
Hadilou1998 <hadilissilame@gmail.com>
2024-09-11 12:15:53 +0200

    Created 4 files and updated CreatorController.php

---
+++ src/Controller/CreatorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('AUTHENTIFICATED_FULLY')]
+class CreatorController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile', methods: ['GET'])]
+    public function profile(): Response
+    {
+        return $this->render('creator/profile.html.twig');
+    }
+
+    #[Route('/profile/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(): Response
+    {
+        return $this->render('creator/edit.html.twig', [
+            'controller_name' => 'CreatorController',
+        ]);
+    } 
+}
