e9ee266e8df750736b5b7a346b75caf86762d737
Hadilou1998 <hadilissilame@gmail.com>
2024-09-11 11:40:07 +0200

    Updated NoteController.php

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +11,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class NoteController extends AbstractController
 {
     #[Route('/', name: 'app_note_all')]
-    public function all(): Response
+    public function all(NoteRepository $nr): Response
     {
-        return $this->render('note/all.html.twig', [
-            'controller_name' => 'NoteController',
+        return $this->render('home/all.html.twig', [
+            'allNotes' => $nr->findBy(['is_public' => true], ['created_at' => 'DESC']),
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_note_show')]
+    public function show(string $slug, NoteRepository $nr): Response
+    {
+        return $this->render('home/show.html.twig', [
+            'note' => $nr->findOneBySlug($slug),
         ]);
     }
 }
