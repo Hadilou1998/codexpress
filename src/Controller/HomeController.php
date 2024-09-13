8b7241e5cb8db3afe125f3f803e6f2dbc90d6617
Hadilou1998 <hadilissilame@gmail.com>
2024-09-13 15:04:21 +0200

    Updated HomeController.php and index.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,13 +12,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(NoteRepository $nr): Response
     {
-        $lastNotes = $nr->findBy(
+        $totalNotes = $nr->findBy(
             ['is_public' => true], // On filtre les notes publiques
             ['created_at' => 'DESC'], // On trie les notes par date de création
             6 // On limite les résultats à 6
         );
         return $this->render('home/index.html.twig', [
-            'lastNotes' => $lastNotes, // On envoie les notes à la vue Twig
+            'totalNotes' => $totalNotes, // On envoie les notes à la vue Twig
         ]);
     }
 }
