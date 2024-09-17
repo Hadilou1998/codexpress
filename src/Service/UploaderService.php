fbc600ba141e20dde395a29ceb6dfb893902ee21
Hadilou1998 <hadilissilame@gmail.com>
2024-09-17 15:44:37 +0200

    Updated UploaderService.php

--- src/Service/UploaderService.php
+++ src/Service/UploaderService.php
@@ -3,7 +3,7 @@
     namespace App\Service;
 
     use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+    use Symfony\Component\HttpFoundation\File\UploadedFile;
 
     /**
      * Service de téléversement de fichiers dans l'application CodeXpress
