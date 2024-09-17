92c98ad6a65bbc06c68629399f4216ae9846c05d
Hadilou1998 <hadilissilame@gmail.com>
2024-09-17 16:31:01 +0200

    Updated CreatorType.php

--- src/Form/CreatorType.php
+++ src/Form/CreatorType.php
@@ -5,9 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Dropzone\Form\DropzoneType;
@@ -20,10 +20,14 @@ class CreatorType extends AbstractType
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
             ->add('image', DropzoneType::class, [
-                'attr' => [
-                    'placeholder' => 'Drag and drop the image here',
-                ],
+                'attr' => ['placeholder' => 'Drag and drop photo here'],
                 'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/gif',],
+                    ]),
+                ]
             ])
             ->add('submit', SubmitType::class)
         ;
