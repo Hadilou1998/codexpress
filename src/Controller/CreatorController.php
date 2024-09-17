5d9d4992dc559c93858c9f0c8642f066f23175c8
Hadilou1998 <hadilissilame@gmail.com>
2024-09-17 16:02:49 +0200

    Updated Project codexpress

--- src/Controller/CreatorController.php
+++ src/Controller/CreatorController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('IS_AUTHENTICATED_FULLY')] // Accès permis uniquement aux utilisateurs authentifiés
+##[IsGranted('IS_AUTHENTIFICATED_FULLY')]
 class CreatorController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile', methods: ['GET'])]
-    public function profile(): Response
+    public function profile(Request $request): Response
     {
         return $this->render('creator/profile.html.twig');
     }
@@ -19,10 +20,9 @@ class CreatorController extends AbstractController
     #[Route('/profile/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
     public function edit(): Response
     {
-        $user = $this->getUser(); // Récupère l'utilisateur authentifié
-        // TODO: Formulaire de modification et traitement des données
+        $user = $this->getUser(); // Récupérer l'utilisateur authentifié
         return $this->render('creator/edit.html.twig', [
-            // TODO: Formulaire à envoyer à la vue Twig
+            'controller_name' => 'CreatorController',
         ]);
-    }
+    } 
 }
