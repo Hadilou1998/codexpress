a81d6ea66103e32f7069a491827fc819aefd2fef
Hadilou1998 <hadilissilame@gmail.com>
2024-09-17 15:24:16 +0200

    Created file UploaderService.php

---
+++ src/Service/UploaderService.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace App\Service;
+
+    /**
+     * Service de téléversement de fichiers dans l'application CodeXpress
+     * - Images (.jpg, .jpeg, .png, .gif)
+     * - Documents (Plus tard)
+     * 
+     * Méthodes : Téléverser, Supprimer
+     */
+    class UploaderService{}
+?>
