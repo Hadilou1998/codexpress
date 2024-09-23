9b6a73b62787a116c0ebb67a5e3112486955ab34
Hadilou1998 <hadilissilame@gmail.com>
2024-09-23 10:56:34 +0200

    Created EmailNotificationService.php

---
+++ src/Service/EmailNotificationService.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace App\Service;
+
+    class EmailNotificationService
+    {
+        public function sendNotification($email, $message)
+        {
+            // Code to send the email notification
+        }
+    }
+?>
