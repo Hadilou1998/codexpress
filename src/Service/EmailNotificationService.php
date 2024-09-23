74a1d6c8c145b4e3de439add74245186472c427b
Hadilou1998 <hadilissilame@gmail.com>
2024-09-23 11:27:30 +0200

    Updated EmailNotificationService.php and Home

--- src/Service/EmailNotificationService.php
+++ src/Service/EmailNotificationService.php
@@ -1,12 +1,35 @@
 <?php
 
-    namespace App\Service;
+namespace App\Service;
 
-    class EmailNotificationService
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class EmailNotificationService
+{
+    public function __construct(private MailerInterface $mailer)
     {
-        public function sendNotification($email, $message)
-        {
-            // Code to send the email notification
+        $this->mailer = $mailer;
+    }
+    
+    public function sendEmail(string $receiver): ?string
+    {
+        try {
+            $email = (new Email())
+                ->from('kakarot@codexpress.fr')
+                ->to($receiver)
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+            $this->mailer->send($email);
+            return 'Email sent successfully';
+        } catch (\Exception $e) {
+            return 'An error occurred while sending the email: ' . $e->getMessage();
         }
     }
+}
 ?>
