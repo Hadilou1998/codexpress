9747220bd059ac0885028ddac8674d5364c124d4
Hadilou1998 <hadilissilame@gmail.com>
2024-09-24 16:09:55 +0200

    Updated files by project codexpress

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,27 +2,30 @@
 
     namespace App\Controller;
 
-use App\Service\EmailNotificationService;
-use App\Service\PaymentService;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\RedirectResponse;
+    use App\Service\EmailNotificationService;
+    use Stripe\Webhook;
+    use App\Service\PaymentService;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Attribute\Route;
+    use Symfony\Component\HttpFoundation\RedirectResponse;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     class SubscriptionController extends AbstractController
     {
         // Route lorsque le paiement est réussi
         #[Route('/payment-success', name: 'app_payment_success', methods: ['GET'])]
         public function paymentSuccess(Request $request, PaymentService $ps, EmailNotificationService $ens): Response
         {
-            if ($request->headers->get('referer') === 'https://checkout.stripe.com/') 
+            if($request->headers->get('referer') === 'https://checkout.stripe.com/' )
             {
                 $subscription = $ps->addSubscription();
                 $ens->sendEmail(
-                    $this->getUser()->getEmail(), 
+                    $this->getUser()->getEmail(),
                     [
-                        'subject' => 'Thank you for your purchase!', 
+                        'subject' => 'Thank you for your purchase!',
                         'template' => 'premium',
                     ]
                 );
@@ -30,28 +33,28 @@ use App\Service\PaymentService;
                     'subscription' => $subscription,
                 ]);
             } else {
-                $this->addFlash('error', 'You can\'t take the subscription without a payment.');
+                $this->addFlash('error', "You can't take a subscription without a payment");
                 return $this->redirectToRoute('app_subscription');
             }
-            
+
             return $this->render('subscription/payment-success.html.twig');
         }
-        
+
         // Route lorsque le paiement a échoué
         #[Route('/payment-cancel', name: 'app_payment_cancel')]
         public function paymentCancel(Request $request): Response
         {
-            if ($request->headers->get('referer') === 'https://checkout.stripe.com/') 
+            if ($request->headers->get('referer') === 'https://checkout.stripe.com/' )
             {
                 return $this->render('subscription/payment-cancel.html.twig');
             } else {
-                $this->addFlash('error', 'You can\'t take the subscription without a payment.');
+                $this->addFlash('error', "You can't take a subscription without a payment");
                 return $this->redirectToRoute('app_subscription');
             }
         }
-        
+
         // Page de présentation de l'abonnement Premium
-        #[Route('/subscription', name: 'app_subscription', methods: ['GET'])]
+        #[Route('/subscription/', name: 'app_subscription', methods: ['GET'])]
         public function index(): Response
         {
             return $this->render('subscription/index.html.twig');
@@ -62,12 +65,35 @@ use App\Service\PaymentService;
          * Ici on utilise la classe RedirectResponse de HttpFoundation
          * Cela nous donne accès à la méthos redirect qui génère la requête
          * à partir de la session initié avec PaymentService->askCheckout()
-         */
-        // Redirection vers le paiement Stripe
+         **/
         #[Route('/subscription/checkout', name: 'app_subscription_checkout', methods: ['GET'])]
         public function checkout(PaymentService $ps): RedirectResponse
         {
             return $this->redirect($ps->askCheckout()->url);
+        }
+
+        /**
+         * Check du statut de paiement
+         */
+        #[Route('/payment-webhook', name: 'app_stripe_webhook', methods: ['GET', 'POST'])]
+        public function stripeWebhook(Request $request): Response
+        {
+            $payload = $request->getContent();
+            $sigHeader = $request->headers->get('Stripe-Signature');
+            $endpointSecret = $this->getParameter('stripe_webhook_secret');
+
+            try {
+                $event = Webhook::constructEvent(
+                    $payload,
+                    $sigHeader,
+                    $endpointSecret
+                );
+                dd($event);
+            } catch (\UnexpectedValueException $e) {
+                return new Response('Invalid payload', 400);
+            } catch (\Stripe\Exception\SignatureVerificationException $e) {
+                return new Response('Invalid signature', 400);
+            }
         }
     }
 
--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -1,86 +1,86 @@
 <?php
 
-namespace App\Service;
+    namespace App\Service;
 
-use Stripe\Stripe;
-use App\Entity\Subscription;
-use Stripe\Checkout\Session;
-use App\Repository\OfferRepository;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Doctrine\ORM\EntityManagerInterface;
+    use Stripe\Stripe;
+    use App\Entity\Subscription;
+    use Stripe\Checkout\Session;
+    use App\Repository\OfferRepository;
+    use Symfony\Bundle\SecurityBundle\Security;
+    use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+    use Doctrine\ORM\EntityManagerInterface;
 
-class PaymentService
-{
-    private $offer, $domain, $apiKey, $user;
-
-    public function __construct(
-        private ParameterBagInterface $parameter,
-        private OfferRepository $or,
-        private readonly Security $security,
-        private EntityManagerInterface $em
-    ) {
-        $this->offer = $or->findOneByName('Premium'); // Récupération de l'offre Premium
-        $this->apiKey = $this->parameter->get('STRIPE_API_SK'); // Récupération de la clé API Stripe
-        $this->domain = 'https://127.0.0.1:8000/en'; // Adresse du domaine
-        $this->user = $security->getUser(); // Récupération de l'utilisateur connecté
-    }
-
-    /**
-     * askCheckout()
-     * Méthode permettant de créer une session de paiement Stripe
-     * @return Stripe\Checkout\Session
-     */
-    // Générer une demande de paiement
-    public function askCheckout(): ?Session
+    class PaymentService
     {
-        Stripe::setApiKey($this->apiKey); // Définition de la clé API Stripe
-        $checkoutSession = Session::create([
-            'line_items' => [[
-                'price_data' => [
-                    'currency' => 'eur',
-                    'unit_amount' => $this->offer->getPrice() * 100, // Stripe utilise des centimes
-                    'product_data' => [ // Les informations du produit sont personnalisables
-                        'name' => $this->offer->getName(),
-                    ],  
+        private $offer, $domain, $apiKey, $user;
+
+        public function __construct(
+            private ParameterBagInterface $parameter,
+            private OfferRepository $or,
+            private readonly Security $security,
+            private EntityManagerInterface $em
+        ) {
+            $this->offer = $or->findOneByName('Premium'); // Récupération de l'offre Premium
+            $this->apiKey = $this->parameter->get('STRIPE_API_SK'); // Récupération de la clé API Stripe
+            $this->domain = 'https://127.0.0.1:8000/en'; // Adresse du domaine
+            $this->user = $security->getUser(); // Récupération de l'utilisateur connecté
+        }
+
+        /**
+         * askCheckout()
+         * Méthode permettant de créer une session de paiement Stripe
+         * @return Stripe\Checkout\Session
+         */
+        // Générer une demande de paiement
+        public function askCheckout(): ?Session
+        {
+            Stripe::setApiKey($this->apiKey); // Définition de la clé API Stripe
+            $checkoutSession = Session::create([
+                'line_items' => [[
+                    'price_data' => [
+                        'currency' => 'eur',
+                        'unit_amount' => $this->offer->getPrice() * 100, // Stripe utilise des centimes
+                        'product_data' => [ // Les informations du produit sont personnalisables
+                            'name' => $this->offer->getName(),
+                        ],  
+                    ],
+                    'quantity' => 1,
+                ]],
+                'mode' => 'payment',
+                'success_url' => $this->domain . '/payment-success',
+                'cancel_url' => $this->domain . '/payment-cancel',
+                'automatic_tax' => [
+                    'enabled' => false,
                 ],
-                'quantity' => 1,
-            ]],
-            'mode' => 'payment',
-            'success_url' => $this->domain . '/payment-success',
-            'cancel_url' => $this->domain . '/payment-cancel',
-            'automatic_tax' => [
-                'enabled' => false,
-            ],
-        ]);
-        
-        return $checkoutSession;
-    }
+            ]);
+            
+            return $checkoutSession;
+        }
 
-    // Traitement du role des utilisateurs
-    public function addSubscription(): ?Subscription
-    {
-        $subscription = new Subscription();
-        $subscription
-            ->setCreator($this->user)
-            ->setOffer($this->offer)
-            ->setStartDate(new \DateTimeImmutable())
-            ->setEndDate(new \DateTimeImmutable('+30 days'))
-            ;
-            $this->em->persist($subscription);
+        // Traitement du role des utilisateurs
+        public function addSubscription(): ?Subscription
+        {
+            $subscription = new Subscription();
+            $subscription
+                ->setCreator($this->user)
+                ->setOffer($this->offer)
+                ->setStartDate(new \DateTimeImmutable())
+                ->setEndDate(new \DateTimeImmutable('+30 days'))
+                ;
+                $this->em->persist($subscription);
+                $this->em->flush();
+            
+            $userRoles = $this->user->getRoles();
+            $userRoles[] = 'ROLE_PREMIUM';
+            $this->user->setRoles($userRoles);
+            $this->em->persist($this->user);
             $this->em->flush();
-        
-        $userRoles = $this->user->getRoles();
-        $userRoles[] = 'ROLE_PREMIUM';
-        $this->user->setRoles($userRoles);
-        $this->em->persist($this->user);
-        $this->em->flush();
 
-        return $subscription;
+            return $subscription;
+        }
+
+        // Génération de la facture
+
+        // Notifications des emails
     }
-
-    // Génération de la facture
-
-    // Notifications des emails
-}
 ?>
